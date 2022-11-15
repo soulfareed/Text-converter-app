426942896d3eda1c3ede595768beb2cf2e5a4544
fareed <fareedzaman501@gmail.com>
2022-11-15 23:49:39 +0530

    add

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -83,12 +83,3 @@ export default function Home(props) {
     </>
   );
 }
-
-import React from "react";
-import { Text, View } from "react-native";
-import { Header } from "./Header";
-import { heading } from "./Typography";
-
-const WelcomeScreen = () => (
-  <>
- 
