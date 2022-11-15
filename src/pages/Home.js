f90e764deaee3116b2e6cf1d024083fecacfb5cf
fareed <fareedzaman501@gmail.com>
2022-11-15 23:36:55 +0530

    sd

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -95,6 +95,4 @@ const WelcomeScreen = () => (
     <Text style={heading}>Step One</Text>
     <Text>Edit App.js to change this screen and turn it into your app.</Text>
     <Text style={heading}>See Your Changes</Text>
-    <Text>Press Cmd + R inside the simulator to reload your app’s code.</Text>
-    <Text style={heading}>Debug</Text>
   
