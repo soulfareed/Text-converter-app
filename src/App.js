ce96329142375bd4f9589d22637b9b0c99dec76f
fareed <fareedzaman501@gmail.com>
2022-11-16 22:02:07 +0530

    add

--- src/App.js
+++ src/App.js
@@ -4,8 +4,8 @@ import About from "./pages/About";
 import Home from "./pages/Home";
 import Alert from "./components/Alert";
 import React, { useState } from "react";
-
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+
 function App() {
   const [mode, setMode] = useState("light");
 
@@ -38,13 +38,10 @@ function App() {
         title="React App"
         aboutText="About"
         mode={mode}
-        toggleMode={toggleMode}
-      />
+        toggleMode={toggleMode}/>
       <Alert alert={alert} />
       <Routes>
-        <Route
-          path="/"
-          element={<Home showAlert={showAlert} />}
+        <Route path="/" element={<Home showAlert={showAlert} />}
           mode={mode}
         ></Route>
         <Route path="/about" element={<About />}></Route>
@@ -54,3 +51,53 @@ function App() {
 }
 
 export default App;
+
+
+
+
+<li class="nav-item nav-items"><a class="nav-link active" href="#" target="_blank">Gallery</a></li>
+<li class="nav-item nav-items"><a class="nav-link active" href="#" target="_blank">Refund Policy</a>
+</li>
+<li class="nav-item nav-items"><a class="nav-link active" href="#" target="_blank">Contact</a></li>
+</ul>
+</div>
+</div>
+</nav><!-- End: Navbar Right Links -->
+<!-- Start: Simple Slider -->
+<div class="simple-slider">
+<!-- Start: Slideshow -->
+<div class="swiper-container">
+<!-- Start: Slide Wrapper -->
+<div class="swiper-wrapper">
+<!-- Start: Slide -->
+<div class="swiper-slide"
+style="background: url(&quot;/assets/img/WhatsApp%20Image%202022-10-31%20at%2011.35.16%20PM.jpeg?h=93fcd026e584998fcdd28dde9f892bb7&quot;) center / cover no-repeat;width: 100%;">
+</div><!-- End: Slide -->
+<!-- Start: Slide -->
+<div class="swiper-slide"
+style="background: url(&quot;/assets/img/WhatsApp%20Image%202022-10-31%20at%2011.27.43%20PM(1).jpeg?h=9b962ab85e047da5ec955662cdb0ec25&quot;) center center / cover no-repeat;">
+</div><!-- End: Slide -->
+<!-- Start: Slide -->
+<div class="swiper-slide"
+style="background: url(&quot;/assets/img/WhatsApp%20Image%202022-10-31%20at%2011.27.45%20PM(1).jpeg?h=ffc4508bc18423bbd789415cea338797&quot;) center no-repeat;/*height: 100%;*/width: 100%;">
+</div><!-- End: Slide -->
+</div><!-- End: Slide Wrapper -->
+<!-- Start: Pagination -->
+<div class="swiper-pagination"></div><!-- End: Pagination -->
+<!-- Start: Previous -->
+<div class="swiper-button-prev"></div><!-- End: Previous -->
+<!-- Start: Next -->
+<div class="swiper-button-next"></div><!-- End: Next -->
+</div><!-- End: Slideshow -->
+</div><!-- End: Simple Slider -->
+<!-- Start: Bold BS4 Footer Big Logo -->
+<footer id="myFooter">
+<div class="container-fluid">
+<div class="row text-center">
+<div class="col-12 col-sm-6 col-md-2">
+<h5>Get started</h5>
+<ul>
+    <li><a href="#">Home</a></li>
+    <li><a href="#">Downloads<br></a></li>
+    <li><a href="#">Sign Up</a></li>
+    <li><a href="#">Othe
